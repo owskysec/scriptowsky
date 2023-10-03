e07e7d833e39d0aaad38f49a7fa648dcc2372a52
Owsky -/N3T1Z3N <58825286+owskysec@users.noreply.github.com>
2023-10-03 18:45:24 +0700

    Create Owsky.php

---
+++ Owsky.php
@@ -0,0 +1,90 @@
+
+<html>
+ <head>
+  <title> PWND? by N3T1Z3N</title> 
+  <meta name="description" content="Don't be afraid.. I'm Friendly.."> 
+  <meta content="â€™N3T1Z3N" IDâ€² name="â€™subjectâ€™/"> 
+  <meta content="â€™Happy" New Year'sâ€² name="â€™Abstractâ€™/"> 
+  <meta content="â€™Hackedâ€²" name="â€™descriptionâ€™/"> 
+  <meta content="â€™PWND?" By OWSKY name="â€™copyrightâ€™/"> 
+  <meta content="â€™PWND?" By OWSKY name="â€™authorâ€™/"> 
+ </head>
+ <body> 
+  <link href="https://fonts.googleapis.com/css?family=Indie+Flower&amp;display=swap" rel="stylesheet">
+  <link href="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgkjghoana2LJUvsMlS8JrM73nk2Gdl5DcT-zypiUqbTq2gjE2" rel="icon" type="image/x-icon">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
+  <style>@import url(https://fonts.googleapis.com/css?family=Indie Flower);
+
+h1 { color: #ffffff; font-size: 25px; font-family: Indie Flower;}
+
+.typed { font-family: Indie Flower; color: #fff;}
+
+.typed-cursor{ opacity: 1; animation: blink 0.7s infinite;}
+
+@keyframes blink{ 0% { opacity:1; } 50% { opacity:0; } 100% { opacity:1; }}</style>
+  <style>html {background-color: #000;}#tulisan { font-family: Indie Flower; color: #fff; font-size: 30px; text-align: center;}.btn { background-color: #000; border: 2px solid #fff; border-radius: 13px; width: 250px; height: 50px;}.blink_me{font-size:60px;-webkit-animation-name:blinker;-webkit-animation-duration:2s;-webkit-animation-timing-function:linear;-webkit-animation-iteration-count:infinite;-moz-animation-name:blinker;-moz-animation-duration:2s;-moz-animation-timing-function:linear;-moz-animation-iteration-count:infinite;animation-name:blinker;animation-duration:1s;animation-timing-function:linear;animation-iteration-count:infinite;}@-moz-keyframes blinker{0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}@-webkit-keyframes blinker{ 0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}@keyframes blinker{0% {opacity:1.0;}50% {opacity:0.0;}100% {opacity:1.0;}}</style>
+  <style>@import url(http://fonts.googleapis.com/css?family=Indie+Flower);.text { width: 100%; height: 100px; margin: auto; display: block; text-align: center;}
+
+.text span { position: relative; top: 20px; display: inline-block; -webkit-animation: bounce .3s ease infinite alternate; -moz-animation: bounce .3s ease infinite alternate; -ms-animation: bounce .3s ease infinite alternate; -o-animation: bounce .3s ease infinite alternate; animation: bounce .3s ease infinite alternate; font-family: 'Indie Flower', cursive; font-size: 50px; color: white; text-shadow: 0 1px 0 #CCC, 0 2px 0 #CCC, 0 3px 0 #CCC, 0 4px 0 #CCC, 0 5px 0 #CCC, 0 6px 0 transparent, 0 7px 0 transparent, 0 8px 0 transparent, 0 9px 0 transparent, 0 10px 10px rgba(0, 0, 0, .4);}
+
+.text span:nth-child(2) {-webkit-animation-delay: .1s;-moz-animation-delay: .1s;animation-delay: .1s;}.text span:nth-child(3) {-webkit-animation-delay: .2s;-moz-animation-delay: .2s;animation-delay: .2s;}.text span:nth-child(4) {-webkit-animation-delay: .3s;-moz-animation-delay: .3s;animation-delay: .3s;}.text span:nth-child(5) {-webkit-animation-delay: .4s;-moz-animation-delay: .4s;animation-delay: .4s;}.text span:nth-child(6) {-webkit-animation-delay: .5s;-moz-animation-delay: .5s;animation-delay: .5s;}.text span:nth-child(7) {-webkit-animation-delay: .6s;-moz-animation-delay: .6s;animation-delay: .6s;}.text span:nth-child(8) {-webkit-animation-delay: .7s;-moz-animation-delay: .7s;animation-delay: .7s;}.text span:nth-child(9) {-webkit-animation-delay: .8s;-moz-animation-delay: .8s;animation-delay: .8s;}.text span:nth-child(10) {-webkit-animation-delay: .9s;-moz-animation-delay: .9s;animation-delay: .9s;}.text span:nth-child(11) {-webkit-animation-delay: 1s;-moz-animation-delay: 1s;animation-delay: 1s;}.text span:nth-child(12) {-webkit-animation-delay: 1.1s;-moz-animation-delay: 1.1s;animation-delay: 1.1s;}.text span:nth-child(13) {-webkit-animation-delay: 1.2s;-moz-animation-delay: 1.2s;animation-delay: 1.2s;}.text span:nth-child(14) {-webkit-animation-delay: 1.3s;-moz-animation-delay: 1.3s;animation-delay: 1.3s;}.text span:nth-child(15) {-webkit-animation-delay: 1.4s;-moz-animation-delay: 1.4s;animation-delay: 1.4s;}.text span:nth-child(16) {-webkit-animation-delay: 1.5s;-moz-animation-delay: 1.5s;animation-delay: 1.5s;}
+
+@-webkit-keyframes bounce { 100% {top: -20px; text-shadow: 0 1px 0 #CCC,0 2px 0 #CCC,0 3px 0 #CCC,0 4px 0 #CCC,0 5px 0 #CCC,0 6px 0 #CCC,0 7px 0 #CCC,0 8px 0 #CCC,0 9px 0 #CCC,0 50px 25px rgba(0, 0, 0, .2);}}
+
+@-moz-keyframes bounce { 100% {top: -20px; text-shadow: 0 1px 0 #CCC,0 2px 0 #CCC,0 3px 0 #CCC,0 4px 0 #CCC,0 5px 0 #CCC,0 6px 0 #CCC,0 7px 0 #CCC,0 8px 0 #CCC,0 9px 0 #CCC,0 50px 25px rgba(0, 0, 0, .2);}}
+
+@keyframes bounce { 100% {top: -20px; text-shadow: 0 1px 0 #CCC,0 2px 0 #CCC,0 3px 0 #CCC,0 4px 0 #CCC,0 5px 0 #CCC,0 6px 0 #CCC,0 7px 0 #CCC,0 8px 0 #CCC,0 9px 0 #CCC,0 50px 25px rgba(0, 0, 0, .2);}} </style>
+
+ <br>
+  <br>
+  <br>
+  <br>
+  <center>
+ <center>
+   <img src="https://media.giphy.com/media/HnKSuvC39SdOM/giphy.gif?ver=6" widht="600" height="550"></span>
+  </center>
+  <br>
+  <br>
+   <center><span class="blink_me"><font face="Indie Flower" size="8" color="red">Stamped? by N3T1Z3N</font> <span class="blink_me"></center>
+    <br>
+    <br>
+  <center>
+   <div id="Tulisan"> 
+   </div> 
+   <br>
+   
+   <script type="text/javascript"> var i=0,Tulisan; Tulisan = "Unhappy Fucking New Year"
+
+function typing() { if(i<Tulisan.length){ document.getElementById("Tulisan").innerHTML += Tulisan.charAt(i); i++; setTimeout(typing,80); } } typing();</script>
+  </center>
+  <br>
+    <br>
+  <br>
+  
+<br>
+    <br>
+  <br>
+  <iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1098628330&amp;color=%23ff00c3&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+   </center>
+ <br>
+    <br>
+  <br>  <center><h1><font face="Courier" size="2"> -/Copyright By N3Towsky 2022-2023</font><span id="typed" class="typed"></span></h1> 
+
+  </center>
+ 
+  <div class="animated flat"> 
+   
+  </div>
+
+ 
+   </center>
+  </div>
+  
+ 
+  <br>
+  <br>
+  <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+ 
+ </body>
+</html>
